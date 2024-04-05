113557dd3a3b968b9add3311f3752633cddad0e4
mstsurnalyakter <surnalyms@gmail.com>
2024-04-05 14:45:38 +0600

    .env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBEDWKMmDnHK-0sJ3vlDIBPzSv5lFDGOaU",
-  authDomain: "dragon-news-64011.firebaseapp.com",
-  projectId: "dragon-news-64011",
-  storageBucket: "dragon-news-64011.appspot.com",
-  messagingSenderId: "7970566471",
-  appId: "1:7970566471:web:44bb99485abcd82f88dc62",
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
